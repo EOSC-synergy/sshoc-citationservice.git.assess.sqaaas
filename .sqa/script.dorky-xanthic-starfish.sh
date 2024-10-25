ca9f3c071fe971b84bf3b56d63f905a1ac89237c
Pablo Orviz <orviz@ifca.unican.es>
2024-10-25 12:36:10 +0200

    Add JePL file structure

---
+++ .sqa/script.dorky-xanthic-starfish.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitea-s2i2s.isti.cnr.it/concordia/sshoc-citationservice &&
+    licensee detect . --confidence 60 --json 
+)
